543258dfaba13369f4d86ed029f803f9e91a524a
WILLBO\willb <willboulter1@gmail.com>
2026-01-18 12:54:48 +0000

    Fix missing test imports

--- src/test/java/com/elastic/runner/ElasticRunnerIntegrationTest.java
+++ src/test/java/com/elastic/runner/ElasticRunnerIntegrationTest.java
@@ -2,6 +2,7 @@ package com.elastic.runner;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
